757b7505b73754e1975d69a86991bcafd0fe561d
filipe83bi <kerubimmusik@gmail.com>
2025-03-19 18:04:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,27 +3,48 @@ const inputFieldElement = document.getElementById("input-task");
 const submitButtonElement = document.getElementById("submit-task-button");
 const taskListElement = document.getElementById("task-list");
 const resetButtonelement = document.getElementById("reset");
+const checkboxElement = document.querySelectorAll(".checkbox");
 
 const createTask = function () {
   const inputValue = inputFieldElement.value;
-  const newTask = `<li><input type="checkbox" id="checkbox"> <span>${inputValue}</span></li>`;
+  const taskWith15digets = inputValue.slice(0, 15);
+  const taskConcat = taskWith15digets.concat();
 
-  if (!inputValue) return;
-  taskListElement.innerHTML += newTask;
+  const li = document.createElement("li");
+  const input = document.createElement("input");
+  input.type = "checkbox";
+  const span = document.createElement("span");
+  span.setAttribute("id", "checked");
+
+  if (!inputValue) {
+    alert("Input should not by empty");
+    return;
+  }
+
   inputFieldElement.value = "";
+  li.appendChild(input);
+  li.appendChild(span);
+  span.innerHTML = taskConcat;
+  console.log(li);
+  taskListElement.appendChild(li);
+};
+
+const resetFunction = function () {
+  taskListElement.innerHTML = "";
 };
 
 submitButtonElement.addEventListener("click", createTask);
 
 document.addEventListener("keypress", (e) => {
-  console.log(e);
   if (e.key === "Enter") {
     createTask();
   }
 });
 
-const resetFunction = function () {
-  taskListElement.innerHTML = "";
-};
+document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
+  if (checkbox.checked) {
+    console.log("checked", checkbox);
+  }
+});
 
 resetButtonelement.addEventListener("click", resetFunction);
